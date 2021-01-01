13d27a5d5b7aafef7c57f134843d781893748948
Wenbo Bian <admin@bianwenbo.onmicrosoft.com>
2021-01-01 23:56:30 +0800

    V2.0 (NG).

--- book/book.js
+++ book/book.js
@@ -2,7 +2,7 @@ function change() {
 //    // Clear Attributes
 //    const elements = document.getElementsByTagName('a');
 //    for (const i = 0; i < elements.length; i++) {
-//        elements[i].style.color = 'black';
+//        elements[i].style.background = 'black';
 //        elements[i].removeAttribute("href");
 //    }
 
@@ -16,118 +16,66 @@ function change() {
     // Description
     // document.getElementById("description").style.display = "block";
 
+    var idUrl = {};
+    idUrl["douban"] = "https://book.douban.com/subject_search?search_text=";
+    idUrl["goodreads"] = "https://www.goodreads.com/search?query=";
+    idUrl["dangdang"] = "http://search.dangdang.com/?ddsale=1&key=";
+    idUrl["amazon"] = "https://www.amazon.com/s?i=stripbooks-intl-ship&k=";
+    idUrl["superlib"] = "http://book.ucdrs.superlib.net/search?allsw=&bCon=&ecode=utf-8&channel=search&Field=1&sw=";
+    idUrl["google"] = "https://www.google.com/search?q=";
+    idUrl["yandex"] = "https://yandex.com/search/?text=";
+    idUrl["baidu"] = "https://www.baidu.com/s?wd=";
+    idUrl["libgen"] = "http://gen.lib.rus.ec/search.php?req=";
+    idUrl["zlibrary"] = "https://b-ok.cc/s/?q=";
+    idUrl["itpanda"] = "https://www.itpanda.net/book/search?query=";
+    idUrl["pdfbook"] = "http://www.pdfbook.cn/?s=";
+    idUrl["shuge"] = "https://new.shuge.org/?s=";
+    idUrl["zhishikoo"] = "https://book.zhishikoo.com/?s=";
+    idUrl["aibooks"] = "https://www.aibooks.cc/search/";
+    idUrl["sxpdf"] = "https://www.sxpdf.com/?s=";
+    idUrl["idata"] = "https://bks.thefuture.top/result.php?q=";
+    idUrl["wikipediazh"] = "https://zh.wikipedia.org/w/index.php?search=";
+    idUrl["wikipedia"] = "https://en.wikipedia.org/w/index.php?search=";
+    idUrl["thefuture"] = "https://bks.thefuture.top/result.php?q=";
+    idUrl["zdict"] = "https://www.zdic.net/hans/";
+    idUrl["ctext"] = "https://ctext.org/searchbooks.pl?if=gb&remap=gb&searchu=";
+
+    for (x in idUrl)
+    {
+        let id = document.getElementById(x);
+        id.style.background="lightcyan";
+        id.href = idUrl[x] + searchValue;
+    }
+
     // Info
-    const douban = document.getElementById("douban");
-    douban.style.color="green";
-    douban.href = "https://book.douban.com/subject_search?search_text=" + searchValue;
-
-    const goodreads = document.getElementById("goodreads");
-    goodreads.style.color="green";
-    goodreads.href = "https://www.goodreads.com/search?query=" + searchValue;
-
-    const dangdang = document.getElementById("dangdang");
-    dangdang.style.color="green";
-    dangdang.href = "http://search.dangdang.com/?ddsale=1&key=" + searchValue;
-
+    idUrl["jd"] = "https://search.jd.com/Search?keyword=";
     const jd = document.getElementById("jd");
-    jd.style.color="green";
-    jd.href = "https://search.jd.com/Search?keyword=" + "\"" + searchValue + "\"";
-
-    const amazon = document.getElementById("amazon");
-    amazon.style.color="green";
-    amazon.href = "https://www.amazon.com/s?i=stripbooks-intl-ship&k=" + searchValue;
-
-    const superlib = document.getElementById("superlib");
-    superlib.style.color="green";
-    superlib.href = "http://book.ucdrs.superlib.net/search?allsw=&bCon=&ecode=utf-8&channel=search&Field=1&sw=" + searchValue;
-
-    const google = document.getElementById("google");
-    google.style.color="green";
-    google.href = "https://www.google.com/search?q=" + searchValue;
-
-    const yandex = document.getElementById("yandex");
-    yandex.style.color="green";
-    yandex.href = "https://yandex.com/search/?text=" + searchValue;
-
-    const baidu = document.getElementById("baidu");
-    baidu.style.color="green";
-    baidu.href = "https://www.baidu.com/s?wd=" + searchValue;
+    jd.style.background="lightcyan";
+    jd.href = idUrl["jd"] + "\"" + searchValue + "\"";
 
     // Download directly
-    const libgen = document.getElementById("libgen");
-    libgen.style.color="green";
-    libgen.href = "http://gen.lib.rus.ec/search.php?req=" + searchValue;
-
-    const zlibrary = document.getElementById("zlibrary");
-    zlibrary.style.color="green";
-    zlibrary.href = "https://b-ok.cc/s/?q=" + searchValue;
-
-    const itpanda = document.getElementById("itpanda");
-    itpanda.style.color="green";
-    itpanda.href = "https://www.itpanda.net/book/search?query=" + searchValue;
-
-    const pdfbook = document.getElementById("pdfbook");
-    pdfbook.style.color="green";
-    pdfbook.href = "http://www.pdfbook.cn/?s=" + searchValue;
-
-    const shuge = document.getElementById("shuge");
-    shuge.style.color="green";
-    shuge.href = "https://new.shuge.org/?s=" + searchValue;
-
-    const zhishikoo = document.getElementById("zhishikoo");
-    zhishikoo.style.color="green";
-    zhishikoo.href = "https://book.zhishikoo.com/?s=" + searchValue;
-
-    const aibooks = document.getElementById("aibooks");
-    aibooks.style.color="green";
-    aibooks.href = "https://www.aibooks.cc/search/" + searchValue;
-
-    const sxpdf = document.getElementById("sxpdf");
-    sxpdf.style.color="green";
-    sxpdf.href = "https://www.sxpdf.com/?s=" + searchValue;
 
     // Online drive
+    idUrl["dalipan"] = "https://www.dalipan.com/search?keyword=";
     const dalipan = document.getElementById("dalipan");
-    dalipan.style.color="green";
-    dalipan.href = "https://www.dalipan.com/search?keyword=" + encodeURIComponent(searchValue);
+    dalipan.style.background="lightcyan";
+    dalipan.href = idUrl["dalipan"] + encodeURIComponent(searchValue);
 
+    idUrl["toplinks"] = "http://www.toplinks.cc/s/?keyword=";
     const toplinks = document.getElementById("toplinks");
-    toplinks.style.color="green";
-    toplinks.href = "http://www.toplinks.cc/s/?keyword=" + encodeURIComponent(searchValue);
+    toplinks.style.background="lightcyan";
+    toplinks.href = idUrl["toplinks"] + encodeURIComponent(searchValue);
 
     // Download limited
+    idUrl["epubee"] = "http://cn.epubee.com/books/?s=";
     const epubee = document.getElementById("epubee");
-    epubee.style.color="green";
+    epubee.style.background="lightcyan";
     const epubeeValue = btoa(encodeURIComponent(searchValue.slice(-1)) + searchValue.length + encodeURIComponent("$" + searchValue.slice(0, searchValue.length - 1) + "#"));
-    epubee.href = "http://cn.epubee.com/books/?s=" + epubeeValue;
+    epubee.href = idUrl["epubee"] + epubeeValue;
 
     // ALTERNATIVE
-    const thefuture = document.getElementById("thefuture");
-    thefuture.style.color="green";
-    thefuture.href = "https://bks.thefuture.top/result.php?q=" + searchValue;
-
     // Article
-    const idata = document.getElementById("idata");
-    idata.style.color="green";
-    idata.href = "https://search.cn-ki.net/search?db=CFLS&keyword=" + searchValue;
-
     // Word
-    const wikipediazh = document.getElementById("wikipediazh");
-    wikipediazh.style.color="green";
-    wikipediazh.href = "https://zh.wikipedia.org/w/index.php?search=" + searchValue;
-
-    const wikipedia = document.getElementById("wikipedia");
-    wikipedia.style.color="green";
-    wikipedia.href = "https://en.wikipedia.org/w/index.php?search=" + searchValue;
-
-    const zdict = document.getElementById("zdict");
-    zdict.style.color="green";
-    zdict.href = "https://www.zdic.net/hans/" + searchValue;
-
-    const ctext = document.getElementById("ctext");
-    ctext.style.color="green";
-    ctext.href = "https://ctext.org/searchbooks.pl?if=gb&remap=gb&searchu=" + searchValue;
-
 }
 
 function gbkEncode(str) {
